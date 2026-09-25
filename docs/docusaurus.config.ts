e7f5d856d3e4c5ecdcfe9ff0146893886f5a24bc
Reimar-Coelho <reimargamer@proton.me>
2026-09-25 11:19:29 -0300

    feat: estrutura para deploy github pages

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -10,10 +10,11 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://agrotech-inteli-ati.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/2026_02_Raiar/',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
